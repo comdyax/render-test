343aed07c06e3b6dced4042c5a8b438e7fab0fb8
comdyax <cody24@tuta.io>
2023-11-08 23:11:56 +0100

    Backend ready

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const nodemon = require('nodemon')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note)
+        response.json(note)
+    else
+        response.status(404).end()
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+const generateID = () => {
+    const maxID = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    return maxID + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    console.log(request.get('Content-Type'))
+    console.log(request.headers)
+
+    const body = request.body
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateID()
+    }
+    console.log(note)
+    notes = notes.concat(note)
+
+    response.json(note)
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint!' })
+}
+
+app.use(unknownEndpoint)
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
