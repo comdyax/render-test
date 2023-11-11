69db7700ffd7f8387766bcf9f42ccc68182a5755
comdyax <cody24@tuta.io>
2023-11-11 13:37:37 +0100

    part3-c

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const nodemon = require('nodemon')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.json())
@@ -14,7 +16,6 @@ const requestLogger = (request, response, next) => {
     console.log('---')
     next()
 }
-
 app.use(requestLogger)
 
 let notes = [
@@ -40,16 +41,15 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note)
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    else
-        response.status(404).end()
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -58,30 +58,27 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
+/**
 const generateID = () => {
     const maxID = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
     return maxID + 1
 }
+*/
 
 app.post('/api/notes', (request, response) => {
-    console.log(request.get('Content-Type'))
-    console.log(request.headers)
-
     const body = request.body
     if (!body.content) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateID()
-    }
-    console.log(note)
-    notes = notes.concat(note)
-
-    response.json(note)
+    })
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 const unknownEndpoint = (request, response) => {
@@ -91,7 +88,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://Cody:${password}@testfso.kflgwcn.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+/**
+const note = new Note({
+  content: 'Note nr 3 - Is Best',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  console.log(result);
+  mongoose.connection.close()
+})
+ */
+
+Note.find({ important: false }).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
