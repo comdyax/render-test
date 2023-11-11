242e3139ecd3dd71f02e81bf01fe7ecc19a5b36c
comdyax <cody24@tuta.io>
2023-11-11 22:44:38 +0100

    Delete and Toggle Function to Backend

--- index.js
+++ index.js
@@ -46,16 +46,28 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
+        if (note)
+            response.json(note)
+        else
+            response.status(404).end()
     })
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id).then(result => {
+        response.status(204).end()
+    })
+        .catch(error => {
+            next(error)
+        })
+    /**
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
     response.status(204).end()
+     */
 })
 
 /**
@@ -64,6 +76,21 @@ const generateID = () => {
     return maxID + 1
 }
 */
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -86,6 +113,19 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
